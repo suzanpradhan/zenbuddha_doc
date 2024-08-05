c03f22984d1860e6e13ee864e354256e6801a216
Bineet Joshi <96242544+joshibineet@users.noreply.github.com>
2024-08-05 17:41:30 +0545

    Initial

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+    unstable_staticImage: true,
+  });
+  
+  module.exports = withNextra();
+  
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    project: {
+      link: 'https://github.com/suzanpradhan/zenbuddha_doc',
+    },
+    logo: <span>Zenbuddha</span>,
+  };
+  
